12af3393c9c7bbff232beea50a4dca5a2423463a
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-10-19 23:42:38 +0700

    step 3, learning picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,26 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true)
+
+public class App implements Runnable {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CommandLine.run(new App(), args);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Hello World!");
     }
 }
