bc8a6f842e9fff6cadebf1e162087dc41faf51b1
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-10-19 23:43:15 +0700

    step 3, learning picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,14 +2,6 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
-
-import java.io.File;
-import java.math.BigInteger;
-import java.nio.file.Files;
-import java.security.MessageDigest;
-import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true)
 
