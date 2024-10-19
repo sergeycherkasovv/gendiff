ce6f27bf76d243a919a8c3717e3040dea916a5d4
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-10-19 21:59:50 +0700

    step 2, connecting the application plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
+    application
     id("java")
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "hexlet.code.App"
+}
 
 repositories {
     mavenCentral()
