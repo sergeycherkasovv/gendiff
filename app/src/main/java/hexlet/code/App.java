dd4bc55c6ec42973b64a6a89f71d5f78389b5f7a
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-10-21 22:13:33 +0700

    step 4, change App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,10 +2,22 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-@Command(name = "gendiff", mixinStandardHelpOptions = true)
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
 
 public class App implements Runnable {
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+    @Parameters(index = "1", paramLabel = "filepath1", description = "path to second file")
+    private String filepath2;
+
+    @Option(names = { "-f", "--format"}, paramLabel = "format",defaultValue = "stylish",
+            description = "output format [default: stylish]")
+    private String format = "stylish";
 
     public static void main(String[] args) {
         CommandLine.run(new App(), args);
