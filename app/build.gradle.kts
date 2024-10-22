30cc9ae39a4d68e927f687e6b1d3de6a026c0ba8
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-10-22 22:56:34 +0700

    step 5, add ReadFile.java, file1.json, file2.json

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,7 @@ repositories {
 }
 
 dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.0")
     implementation("info.picocli:picocli:4.7.6")
     annotationProcessor("info.picocli:picocli-codegen:4.7.6")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,15 +1,19 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.util.Map;
+
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
 
-public class App implements Runnable {
+public class App {
     @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
     private String filepath1;
     @Parameters(index = "1", paramLabel = "filepath1", description = "path to second file")
@@ -19,12 +23,16 @@ public class App implements Runnable {
             description = "output format [default: stylish]")
     private String format = "stylish";
 
-    public static void main(String[] args) {
-        CommandLine.run(new App(), args);
+    public static Map<String, Object> objectMapper (String fileName) throws Exception {
+        var json = ReadFile.readFilePath(fileName);
+
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> map = mapper.readValue(json, new TypeReference<Map<String,Object>>(){});
+        return map;
     }
 
-    @Override
-    public void run() {
-        System.out.println("Hello World!");
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
