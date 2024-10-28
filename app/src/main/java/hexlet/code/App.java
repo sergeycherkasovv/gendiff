73cf7c03d83b37a3cce6a5b8375ce1939943bcac
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-10-28 21:27:20 +0700

    step 6, add Differ

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,34 +1,39 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-import java.util.Map;
-
+import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
 
-public class App {
+public class App implements Callable<String> {
     @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
     private String filepath1;
     @Parameters(index = "1", paramLabel = "filepath1", description = "path to second file")
     private String filepath2;
 
-    @Option(names = { "-f", "--format"}, paramLabel = "format",defaultValue = "stylish",
+    @Option(names = {"-f", "--format"}, paramLabel = "format", defaultValue = "stylish",
             description = "output format [default: stylish]")
-    private String format = "stylish";
+    private String format;
 
-    public static Map<String, Object> objectMapper (String fileName) throws Exception {
-        var json = ReadFile.readFilePath(fileName);
+    @Override
+    public String call() throws Exception {
+        var fileRead1 = ReadFile.readFilePath(filepath1);
+        var fileRead2 = ReadFile.readFilePath(filepath2);
+        var fileInMap1 = FileInMap.objectMapper(fileRead1);
+        var fileInMap2 = FileInMap.objectMapper(fileRead2);
 
-        ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> map = mapper.readValue(json, new TypeReference<Map<String,Object>>(){});
-        return map;
+        if (fileInMap1.isEmpty() || fileInMap2.isEmpty()) {
+            throw new Exception();
+        }
+
+        var result = Differ.generate(fileInMap1, fileInMap2, format);
+        System.out.println(result);
+        return result;
     }
 
     public static void main(String... args) {
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,51 @@
+package hexlet.code;
+
+import java.util.*;
+
+public class Differ {
+    private static final String MINUS = "-";
+    private static final String PLUS = "+";
+
+    public static String generate(Map<String, Object> filePath1, Map<String, Object> filePath2, String formatName) throws Exception {
+        List<String> result = new ArrayList<>();
+
+        List<String> listKey = new ArrayList<>(filePath1.keySet());
+        listKey.addAll(filePath2.keySet());
+
+        listKey.stream()
+                .distinct()
+                .sorted()
+                .forEach(key -> {
+                    if (filePath1.containsKey(key) && !filePath2.containsKey(key)) {
+                        result.add(MINUS + " " + key + ": " + (filePath1.get(key)).toString());
+                    } else if (!filePath1.containsKey(key) && filePath2.containsKey(key)) {
+                        result.add(PLUS + " " + key + ": " + filePath2.get(key).toString());
+                    } else if (filePath1.get(key).equals(filePath2.get(key))) {
+                        result.add(" " + " " + key + ": " + filePath1.get(key).toString());
+                    } else if (!filePath1.get(key).equals(filePath2.get(key))) {
+                        result.add(MINUS + " " + key + ": " + filePath1.get(key).toString());
+                        result.add(PLUS + " " + key + ": " + filePath2.get(key).toString());
+                    }
+                });
+
+        return show(result);
+    }
+
+    public static String generate(Map<String, Object> filePath1, Map<String, Object> filePath2) throws Exception {
+        return generate(filePath1, filePath2, "String formatName");
+    }
+
+    public static String show(List<String> list) {
+        var result = new StringBuilder();
+
+        result.append("{");
+        for (var item : list) {
+                result.append("\n");
+                result.append("  " + item);
+            }
+        result.append("\n");
+        result.append("}");
+
+        return result.toString();
+    }
+}
