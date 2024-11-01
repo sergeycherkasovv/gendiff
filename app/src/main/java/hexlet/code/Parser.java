22993d5dabda4e69147ddcca3e79e64c23c323e0
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-11-01 21:45:17 +0700

    step 8, change Parser.java

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -2,7 +2,6 @@ package hexlet.code;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 import java.util.Map;
@@ -10,24 +9,18 @@ import java.util.Map;
 public class Parser {
     public static Map<String, Object> run(String fileName) throws Exception {
         var read = ReadFile.readFilePath(fileName);
-        if(fileName.contains(".yml")) {
-            return parseYaml(read);
-        }
-        if(fileName.contains(".json")) {
-            return parseJson(read);
-        }
-        return Map.of();
+
+        return switch (fileName.substring(fileName.lastIndexOf("."))) {
+            case ".json" -> getParse(read, new ObjectMapper());
+            case ".yml", ".yaml" -> getParse(read, new YAMLMapper());
+            default ->
+                    throw new Exception("Unexpected value: "
+                            + fileName.substring(fileName.lastIndexOf(".")));
+        };
     }
-    public static Map<String, Object> parseYaml(String fileName) throws Exception {
-        ObjectMapper mapper = new YAMLMapper();
-        Map<String, Object> map = mapper.readValue(fileName, new TypeReference<Map<String, Object>>() {
+    public static Map<String, Object> getParse(String fileName, ObjectMapper mapper) throws Exception {
+        return mapper.readValue(fileName,
+                new TypeReference<Map<String, Object>>() {
         });
-        return map;
-    }
-    public static Map<String, Object> parseJson(String fileName) throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> map = mapper.readValue(fileName, new TypeReference<Map<String, Object>>() {
-        });
-        return map;
     }
 }
