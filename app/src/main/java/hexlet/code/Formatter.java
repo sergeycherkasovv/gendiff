7f69ab2f9d96fe63941573477c1e319bb682db11
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-11-04 22:25:17 +0700

    step 10, changed Formatter.java

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -1,6 +1,7 @@
 package hexlet.code;
 
 import hexlet.code.formatter.Json;
+import hexlet.code.formatter.Plain;
 import hexlet.code.formatter.Stylish;
 
 import java.util.Map;
@@ -9,7 +10,7 @@ public class Formatter {
     public static String formatter(Map<String, Map<String, Object>> differMap, String format) throws Exception {
         return switch (format) {
             case "stylish" -> Stylish.getStylish(differMap);
-            case "plain" -> "Plain.plain";
+            case "plain" -> Plain.getPlain(differMap);
             case "json" -> Json.getJson(differMap);
             default -> throw new Exception("Unexpected value: " + format);
         };
