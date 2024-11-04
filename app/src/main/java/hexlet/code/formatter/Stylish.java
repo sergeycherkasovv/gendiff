74cfd1b2b61aa621a017ed6713a5daf4efe27e03
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-11-04 22:25:03 +0700

    step 10, changed Stylish.java

--- app/src/main/java/hexlet/code/formatter/Stylish.java
+++ app/src/main/java/hexlet/code/formatter/Stylish.java
@@ -7,21 +7,29 @@ import java.util.Map;
 public class Stylish {
     private static final String MINUS = "-";
     private static final String PLUS = "+";
+    private static final String OLD = "old";
+    private static final String NEW = "new";
+    private static final String INTACT = "intact";
+    private static final Integer COUNT_EMPTY_LINE = 2;
 
     public static String getStylish(Map<String, Map<String, Object>> list) {
         List<String> result = new ArrayList<>();
+        var emptyString = " ".repeat(COUNT_EMPTY_LINE);
 
         result.add("{");
         list.forEach((key, value) -> {
-            if (value.containsKey(PLUS) && value.containsKey(MINUS)) {
-                result.add(MINUS + "  " + key + ": " + value.get(MINUS));
-                result.add(PLUS + "  " + key + ": " + value.get(PLUS));
-            } else if (value.containsKey(MINUS)) {
-                result.add(MINUS + "  " + key + ": " + value.get(MINUS));
-            } else if (value.containsKey(PLUS)) {
-                result.add(PLUS + "  " + key + ": " + value.get(PLUS));
-            } else {
-                result.add("   " + key + ": " + value.get(" "));
+            var plus = PLUS + emptyString + key + ": " + value.get(NEW);
+            var minus = MINUS + emptyString + key + ": " + value.get(OLD);
+
+            if (value.containsKey(NEW) && value.containsKey(OLD)) {
+                result.add(minus);
+                result.add(plus);
+            } else if (value.containsKey(OLD)) {
+                result.add(minus);
+            } else if (value.containsKey(NEW)) {
+                result.add(plus);
+            } else if (value.containsKey(INTACT)) {
+                result.add(" " + emptyString + key + ": " + value.get(INTACT));
             }
 
         });
