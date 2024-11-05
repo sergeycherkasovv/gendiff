2a626bd0e34134b30652cd6bb3da291416380fa5
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-11-05 16:49:38 +0700

    step 10, changed Parser.java

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -7,18 +7,18 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import java.util.Map;
 
 public class Parser {
-    public static Map<String, Object> run(String fileName) throws Exception {
+    public static Map<String, Object> getParser(String fileName) throws Exception {
         var read = ReadFile.readFilePath(fileName);
 
         return switch (fileName.substring(fileName.lastIndexOf("."))) {
-            case ".json" -> getParse(read, new ObjectMapper());
-            case ".yml", ".yaml" -> getParse(read, new YAMLMapper());
+            case ".json" -> run(read, new ObjectMapper());
+            case ".yml", ".yaml" -> run(read, new YAMLMapper());
             default -> throw new Exception("Unexpected value: "
                     + fileName.substring(fileName.lastIndexOf(".")));
         };
     }
 
-    public static Map<String, Object> getParse(String fileName, ObjectMapper mapper) throws Exception {
+    public static Map<String, Object> run(String fileName, ObjectMapper mapper) throws Exception {
         return mapper.readValue(fileName,
                 new TypeReference<Map<String, Object>>() {
                 });
