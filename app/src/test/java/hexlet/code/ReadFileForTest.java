e99c8bd3ae4b065da8da266bd5f35fd7ef425601
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-11-05 23:10:41 +0700

    changed

---
+++ app/src/test/java/hexlet/code/ReadFileForTest.java
@@ -0,0 +1,23 @@
+package hexlet.code;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ReadFileForTest {
+    private static Path filePath(String fileName) throws Exception {
+        Path path = Paths.get("src", "test", "resources", "fixtures", fileName)
+                .toAbsolutePath().normalize();
+
+        if (!Files.exists(path)) {
+            throw new Exception("File '" + path + "' does not exist");
+        }
+        return path;
+    }
+
+    public static String readFilePath(String fileName) throws Exception {
+        var path = filePath(fileName);
+        return Files.readString(path).trim();
+    }
+
+}
