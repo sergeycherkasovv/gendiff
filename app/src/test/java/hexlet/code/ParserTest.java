ad60ef698463ffacd5acf897eba5e67112fe2ad1
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-11-05 16:49:53 +0700

    step 10, changed ParserTest.java

--- app/src/test/java/hexlet/code/ParserTest.java
+++ app/src/test/java/hexlet/code/ParserTest.java
@@ -1,7 +1,6 @@
 package hexlet.code;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -16,7 +15,7 @@ class ParserTest {
     private static Map<String, Object> fileExpecteds = new HashMap<>();
 
     @BeforeAll
-    public static void main() {
+    public static void beforeAll() {
         Map<String, Object> expecteds = Map.of("host", "hexlet.io",
                 "timeout", 50,
                 "proxy", "123.234.53.22",
@@ -26,8 +25,8 @@ class ParserTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"empty.json", "empty.yml"})
-    void runTest(String file) throws Exception {
-        var output = Parser.run(file);
+    void getParserTest(String file) throws Exception {
+        var output = Parser.getParser(file);
 
         assertTrue(output.isEmpty());
         assertFalse(output.containsKey("hexlet"));
@@ -35,9 +34,9 @@ class ParserTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"file1.json", "file1.yml"})
-    void runTest2(String file) throws Exception {
+    void getParserTest2(String file) throws Exception {
         var actual = fileExpecteds;
-        var output = Parser.run(file);
+        var output = Parser.getParser(file);
         output.forEach((key, value) -> {
             assertEquals(output.get(key), actual.get(key));
         });
@@ -45,8 +44,8 @@ class ParserTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"file1.json", "file1.yml"})
-    void runTest3(String file) throws Exception {
-        var output = Parser.run(file);
+    void getParserTest3(String file) throws Exception {
+        var output = Parser.getParser(file);
 
         assertFalse(output.isEmpty());
         assertTrue(output.containsKey("timeout"));
