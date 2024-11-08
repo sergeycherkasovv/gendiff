d4d47891a91c189b9ac0b698b8d9b078b2480c59
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-11-08 20:48:36 +0700

    changed DifferFilter.java

--- app/src/main/java/hexlet/code/DifferFilter.java
+++ app/src/main/java/hexlet/code/DifferFilter.java
@@ -1,12 +1,16 @@
 package hexlet.code;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.TreeMap;
 
 public class DifferFilter {
     private static final String OLD = "old";
     private static final String NEW = "new";
     private static final String UNCHANGED = "unchanged";
-    //private static final String UNCHANGED = "unchanged";
 
     public static Map<String, Map<String, Object>> getDifferFilter(Map<String, Object> filePath1,
                                                                    Map<String, Object> filePath2) {
@@ -24,7 +28,7 @@ public class DifferFilter {
                         //filePath1.containsValue(filePath2.get(key)) && filePath2.containsValue(filePath1.get(key))
                         map.put(UNCHANGED, filePath2.get(key));
                         result.put(key, map);
-                    }  else if ((filePath1.containsKey(key) && filePath2.containsKey(key))) {
+                    } else if ((filePath1.containsKey(key) && filePath2.containsKey(key))) {
                         map.put(OLD, filePath1.get(key));
                         map.put(NEW, filePath2.get(key));
                         result.put(key, map);
@@ -37,30 +41,5 @@ public class DifferFilter {
                     }
                 });
         return result;
-
-//        listKey.stream()
-//                .distinct()
-//                .sorted()
-//                .forEach(key -> {
-//                    Map<String, Object> map = new HashMap<>();
-//                    if ((filePath1.containsKey(key) && filePath2.containsKey(key))
-//                            && (filePath1.containsValue(filePath2.get(key)) == false
-//                            && filePath2.containsValue(filePath1.get(key)) == false)) {
-//                        map.put(OLD, filePath1.get(key));
-//                        map.put(NEW, filePath2.get(key));
-//                        result.put(key, map);
-//                    } else if ((filePath1.containsKey(key) && filePath2.containsKey(key))
-//                            && filePath1.containsValue(filePath2.get(key)) == true
-//                            && filePath2.containsValue(filePath1.get(key)) == true) {
-//                        map.put(UNCHANGED, filePath2.get(key));
-//                        result.put(key, map);
-//                    } else if(filePath1.containsKey(key) == false && filePath2.containsKey(key) == true) {
-//                        map.put(NEW, filePath2.get(key));
-//                        result.put(key, map);
-//                    } else if (filePath1.containsKey(key) == true && filePath2.containsKey(key) == false) {
-//                        map.put(OLD, filePath1.get(key));
-//                         result.put(key, map);
-//                    }});
-//        return result;
     }
 }
