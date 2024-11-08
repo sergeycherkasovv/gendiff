aa528c3750deb7fc3aa186dbcae68b0c31faaf73
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-11-08 20:53:51 +0700

    changed App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -10,7 +10,7 @@ import java.util.concurrent.Callable;
 @Command(name = "gendiff", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
 
-public class App implements Callable<String> {
+public final class App implements Callable<String> {
     @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
     private String filepath1;
     @Parameters(index = "1", paramLabel = "filepath1", description = "path to second file")
