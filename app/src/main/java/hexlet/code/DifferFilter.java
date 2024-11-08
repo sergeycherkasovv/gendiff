c85cd8f9c9cc1e0dfb188e942ecc8e0c355b1ff8
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-11-08 19:49:26 +0700

    changed DifferFilter.java and Stylish.java

--- app/src/main/java/hexlet/code/DifferFilter.java
+++ app/src/main/java/hexlet/code/DifferFilter.java
@@ -5,12 +5,8 @@ import java.util.*;
 public class DifferFilter {
     private static final String OLD = "old";
     private static final String NEW = "new";
-    private static final String INTACT = "intact";
-
-    private static final String ADDED = "added;";
-    private static final String DELETED = "deleted";
-    private static final String CHANGED = "changed";
     private static final String UNCHANGED = "unchanged";
+    //private static final String UNCHANGED = "unchanged";
 
     public static Map<String, Map<String, Object>> getDifferFilter(Map<String, Object> filePath1,
                                                                    Map<String, Object> filePath2) {
@@ -26,7 +22,7 @@ public class DifferFilter {
                     Map<String, Object> map = new HashMap<>();
                     if (Objects.equals(filePath1.get(key), filePath2.get(key))) {
                         //filePath1.containsValue(filePath2.get(key)) && filePath2.containsValue(filePath1.get(key))
-                        map.put(INTACT, filePath2.get(key));
+                        map.put(UNCHANGED, filePath2.get(key));
                         result.put(key, map);
                     }  else if ((filePath1.containsKey(key) && filePath2.containsKey(key))) {
                         map.put(OLD, filePath1.get(key));
@@ -56,7 +52,7 @@ public class DifferFilter {
 //                    } else if ((filePath1.containsKey(key) && filePath2.containsKey(key))
 //                            && filePath1.containsValue(filePath2.get(key)) == true
 //                            && filePath2.containsValue(filePath1.get(key)) == true) {
-//                        map.put(INTACT, filePath2.get(key));
+//                        map.put(UNCHANGED, filePath2.get(key));
 //                        result.put(key, map);
 //                    } else if(filePath1.containsKey(key) == false && filePath2.containsKey(key) == true) {
 //                        map.put(NEW, filePath2.get(key));
--- app/src/main/java/hexlet/code/formatter/Stylish.java
+++ app/src/main/java/hexlet/code/formatter/Stylish.java
@@ -9,7 +9,7 @@ public class Stylish {
     private static final String PLUS = "+";
     private static final String OLD = "old";
     private static final String NEW = "new";
-    private static final String INTACT = "intact";
+    private static final String UNCHANGED = "unchanged";
     private static final Integer COUNT_EMPTY_LINE = 2;
 
     public static String getStylish(Map<String, Map<String, Object>> list) {
@@ -18,8 +18,8 @@ public class Stylish {
 
         result.add("{");
         list.forEach((key, value) -> {
-            var plus = PLUS + emptyString + key + ": " + value.get(NEW);
-            var minus = MINUS + emptyString + key + ": " + value.get(OLD);
+            var plus = emptyString + PLUS + emptyString + key + ": " + value.get(NEW);
+            var minus = emptyString + MINUS + emptyString + key + ": " + value.get(OLD);
 
             if (value.containsKey(NEW) && value.containsKey(OLD)) {
                 result.add(minus);
@@ -28,8 +28,8 @@ public class Stylish {
                 result.add(minus);
             } else if (value.containsKey(NEW)) {
                 result.add(plus);
-            } else if (value.containsKey(INTACT)) {
-                result.add(" " + emptyString + key + ": " + value.get(INTACT));
+            } else if (value.containsKey(UNCHANGED)) {
+                result.add(emptyString + " " + emptyString + key + ": " + value.get(UNCHANGED));
             }
 
         });
