6ce29e36174509815deb136eb2b696ca43275bdc
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-11-13 20:37:14 +0700

    changed Parser.java

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -7,14 +7,13 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import java.util.Map;
 
 public class Parser {
-    public static Map<String, Object> getParser(String fileName) throws Exception {
-        var read = ReadFile.readFilePath(fileName);
+    public static Map<String, Object> getParser(String fileRead, String fileFormat) throws Exception {
 
-        return switch (fileName.substring(fileName.lastIndexOf("."))) {
-            case ".json" -> run(read, new ObjectMapper());
-            case ".yml", ".yaml" -> run(read, new YAMLMapper());
-            default -> throw new Exception("Unexpected value: "
-                    + fileName.substring(fileName.lastIndexOf(".")));
+        return switch (fileFormat) {
+            case ".json" -> run(fileRead, new ObjectMapper());
+            case ".yml", ".yaml" -> run(fileRead, new YAMLMapper());
+            default -> throw new RuntimeException("Unexpected value: "
+                    + fileRead.substring(fileRead.lastIndexOf(".")));
         };
     }
 
