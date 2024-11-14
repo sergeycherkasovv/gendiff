5dc6e23e6faf79192a03b21c02af07cc86b395c6
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-11-14 19:49:14 +0700

    changed Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,18 +1,17 @@
 package hexlet.code;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Differ {
 
     public static String generate(String filepath1,
                                   String filepath2,
                                   String formatName) throws Exception {
-        var fileParse1 = Parser.getParser(filepath1);
-        var fileParse2 = Parser.getParser(filepath2);
 
-        if (fileParse1.isEmpty()) {
-            throw new Exception("The file " + "'" + filepath1 + "'" + " is empty");
-        } else if (fileParse2.isEmpty()) {
-            throw new Exception("The file " + "'" + filepath2 + "'" + " is empty");
-        }
+        var fileParse1 = readAndParse(filepath1);
+        var fileParse2 = readAndParse(filepath2);
 
         var result = DifferFilter.getDifferFilter(fileParse1, fileParse2);
         return Formatter.getFormatter(result, formatName);
@@ -21,5 +20,13 @@ public class Differ {
     public static String generate(String filepath1,
                                   String filepath2) throws Exception {
         return generate(filepath1, filepath2, "stylish");
+    }
+
+
+    public static Map<String, Object> readAndParse(String file) throws Exception {
+        var read = ReadFile.readFilePath(file);
+        var fileFormat = file.substring(file.lastIndexOf("."));
+        return Parser.getParser(read, fileFormat);
+
     }
 }
