a3fe3715ecbec71c6d0614ccfd1230fbba843047
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-11-16 19:07:21 +0700

    changed Json.java

--- app/src/main/java/hexlet/code/formatter/Json.java
+++ app/src/main/java/hexlet/code/formatter/Json.java
@@ -1,11 +1,13 @@
 package hexlet.code.formatter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.List;
 import java.util.Map;
 
 public class Json {
-    public static String getJson(Map<String, Map<String, Object>> list) throws Exception {
+    public static String getJson(List<Map<String, Object>> list) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.writeValueAsString(list);
     }
