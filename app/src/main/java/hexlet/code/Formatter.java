729d01a5afb680be8bce1123e8508df35e4824bc
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-11-16 19:06:49 +0700

    changed Formatter.java

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -4,15 +4,16 @@ import hexlet.code.formatter.Json;
 import hexlet.code.formatter.Plain;
 import hexlet.code.formatter.Stylish;
 
+import java.util.List;
 import java.util.Map;
 
 public class Formatter {
-    public static String getFormatter(Map<String, Map<String, Object>> differMap, String format) throws Exception {
+    public static String getFormatter(List<Map<String, Object>> differMap, String format) throws Exception {
         return switch (format) {
             case "stylish" -> Stylish.getStylish(differMap);
             case "plain" -> Plain.getPlain(differMap);
             case "json" -> Json.getJson(differMap);
-            default -> throw new Exception("Unexpected value: " + format);
+            default -> throw new RuntimeException("Unexpected value: " + format);
         };
     }
 }
