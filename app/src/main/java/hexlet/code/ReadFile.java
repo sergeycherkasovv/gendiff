2bb7e5331e65745bb35ac72c6af66002756ce328
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-11-16 19:34:50 +0700

    changed ReadFile.java

--- app/src/main/java/hexlet/code/ReadFile.java
+++ app/src/main/java/hexlet/code/ReadFile.java
@@ -5,18 +5,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ReadFile {
-    private static Path filePath(String fileName) throws Exception {
-        Path path;
-
-        if (fileName.contains("/")) {
-            path = Paths.get(fileName)
-                    .toAbsolutePath().normalize();
-        } else {
-            path = Paths.get("src", "main", "resources", fileName)
-                    .toAbsolutePath().normalize();
-        }
-
-        return path;
+    private static Path filePath(String fileName) {
+        return Paths.get(fileName)
+                .toAbsolutePath().normalize();
     }
 
     public static String readFilePath(String fileName) throws Exception {
