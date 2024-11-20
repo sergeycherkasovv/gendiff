7f1557c75b1e592dc24a13ed375f2f8628bb76a4
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-11-20 21:43:10 +0700

    changed FileDifferences.java

--- app/src/main/java/hexlet/code/FileDifferences.java
+++ app/src/main/java/hexlet/code/FileDifferences.java
@@ -7,19 +7,8 @@ import java.util.Map;
 import java.util.Objects;
 
 public class FileDifferences {
-    //public static final String STATUS = "status";
-    public static final String CHANGED = "changed";
-    public static final String NEW = "new";
-    public static final String SAME = "same";
-    public static final String DELETED = "deleted";
 
-    public static final String KEY = "key";
-    public static final String VALUE_ONE = "valueOne";
-    public static final String VALUE_SECOND = "valueSecond";
-
-
-
-    public static List<Map<String, Object>> getDifferences(Map<String, Object> fileParse1,
+    public static List<Map<Object, Object>> getDifferences(Map<String, Object> fileParse1,
                                                            Map<String, Object> fileParse2) {
 
         List<String> listKey = new ArrayList<>();
@@ -30,24 +19,24 @@ public class FileDifferences {
                 .distinct()
                 .sorted()
                 .map(key -> {
-                    Map<String, Object> map = new HashMap<>();
+                    Map<Object, Object> map = new HashMap<>();
                     if (Objects.equals(fileParse1.get(key), fileParse2.get(key))) {
-                        map.put(DiffConst.STATUS.toString(), DiffConst.SAME);
-                        map.put(DiffConst.KEY.toString(), key);
-                        map.put(DiffConst.VALUE_SECOND.toString(), fileParse2.get(key));
+                        map.put(DiffConst.STATUS, DiffConst.SAME);
+                        map.put(DiffConst.KEY, key);
+                        map.put(DiffConst.VALUE_SECOND, fileParse2.get(key));
                     } else if ((fileParse1.containsKey(key) && fileParse2.containsKey(key))) {
-                        map.put(DiffConst.STATUS.toString(), DiffConst.CHANGED);
-                        map.put(DiffConst.KEY.toString(), key);
-                        map.put(DiffConst.VALUE_ONE.toString(), fileParse1.get(key));
-                        map.put(DiffConst.VALUE_SECOND.toString(), fileParse2.get(key));
+                        map.put(DiffConst.STATUS, DiffConst.CHANGED);
+                        map.put(DiffConst.KEY, key);
+                        map.put(DiffConst.VALUE_ONE, fileParse1.get(key));
+                        map.put(DiffConst.VALUE_SECOND, fileParse2.get(key));
                     } else if (fileParse1.containsKey(key) && !fileParse2.containsKey(key)) {
-                        map.put(DiffConst.STATUS.toString(), DiffConst.DELETED);
-                        map.put(DiffConst.KEY.toString(), key);
-                        map.put(DiffConst.VALUE_ONE.toString(), fileParse1.get(key));
+                        map.put(DiffConst.STATUS, DiffConst.DELETED);
+                        map.put(DiffConst.KEY, key);
+                        map.put(DiffConst.VALUE_ONE, fileParse1.get(key));
                     } else if (fileParse2.containsKey(key) && !fileParse1.containsKey(key)) {
-                        map.put(DiffConst.STATUS.toString(), DiffConst.NEW);
-                        map.put(DiffConst.KEY.toString(), key);
-                        map.put(DiffConst.VALUE_SECOND.toString(), fileParse2.get(key));
+                        map.put(DiffConst.STATUS, DiffConst.NEW);
+                        map.put(DiffConst.KEY, key);
+                        map.put(DiffConst.VALUE_SECOND, fileParse2.get(key));
                     }
                     return map;
                 })
