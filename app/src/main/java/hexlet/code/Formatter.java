45def6bc8813f06edcbd4bc5b83c4a0c84e1ec0e
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-11-23 19:43:46 +0700

    changed Formatter.java

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import hexlet.code.constants.Keys;
 import hexlet.code.formatter.Json;
 import hexlet.code.formatter.Plain;
 import hexlet.code.formatter.Stylish;
@@ -8,7 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 public class Formatter {
-    public static String getFormatter(List<Map<Object, Object>> differMap, String format) throws Exception {
+    public static String getFormatter(List<Map<Keys, Object>> differMap, String format) throws Exception {
         return switch (format) {
             case "stylish" -> Stylish.getStylish(differMap);
             case "plain" -> Plain.getPlain(differMap);
